ec6c0aa55f306dadbeb29e0ebcb21aa6b6850655
Nam Tran <namtran0108642@gmail.com>
2024-10-18 11:42:48 +0700

    cúp điện

--- Frontend/simple-web-app/src/app/app.component.ts
+++ Frontend/simple-web-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
+import { EventComponent } from "../event-component/event-component.component";
 
 @Component({
-  selector: 'app-root', // Selector của AppComponent, không phải RegisterComponent
+  selector: 'app-root', // Selector của AppComponent
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RegisterComponent] // Import RegisterComponent
+  imports: [EventComponent, RegisterComponent] // Import cả EventComponent và RegisterComponent
 })
 export class AppComponent {}
--- Frontend/simple-web-app/src/event.service.ts
+++ Frontend/simple-web-app/src/event.service.ts
@@ -1,7 +1,7 @@
-// event.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'; // Import map
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,13 @@ export class EventService {
 
   constructor(private http: HttpClient) { }
 
-  // Lấy tất cả sự kiện
-  getEvents(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  // Lấy tất cả sự kiện và chỉ lấy lastname và firstname
+  getEvents(): Observable<{ firstname: string, lastname: string }[]> {
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map(events => events.map(event => ({
+        firstname: event.firstname,
+        lastname: event.lastname
+      })))
+    );
   }
 }
