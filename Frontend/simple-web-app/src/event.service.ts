9856bea98be1b983318f714a13b652015967d982
Nam Tran <namtran0108642@gmail.com>
2024-10-18 10:49:08 +0700

    test get 2

--- Frontend/simple-web-app/src/event.service.ts
+++ Frontend/simple-web-app/src/event.service.ts
@@ -1,9 +1,18 @@
+// event.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+  private apiUrl = 'https://localhost:7155/api/events'; // Địa chỉ API của bạn
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Lấy tất cả sự kiện
+  getEvents(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
